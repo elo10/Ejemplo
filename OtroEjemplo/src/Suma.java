249fbf27c7981edbebe9c0d745eb90434234c63f
Eloy <Eloy@192.168.1.38>
2016-02-24 17:57:02 +0100

    Una Suma

---
+++ OtroEjemplo/src/Suma.java
@@ -0,0 +1,14 @@
+
+public class Suma {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 10;
+		int valor2 = 20;
+		
+		int result = valor1 + valor2;
+		
+		System.out.print("La suma es: " + result);
+		
+	}
+}
